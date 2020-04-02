839946721a60e08769dde62ab912255db7a4e29e
Joaquin Fernandez <copako12@gmail.com>
2020-04-02 19:15:37 -0300

    Updates

---
+++ precache-manifest.5df54f6acf39bd0b6408af7b682b6fa2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d6e60b727022b4486c9ae2dd7fa5253",
+    "url": "/sushi-react/index.html"
+  },
+  {
+    "revision": "405749552c1b76fa8ed0",
+    "url": "/sushi-react/static/css/main.33b7dc5f.chunk.css"
+  },
+  {
+    "revision": "09a6c9f827135465de01",
+    "url": "/sushi-react/static/js/2.13d404f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sushi-react/static/js/2.13d404f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "405749552c1b76fa8ed0",
+    "url": "/sushi-react/static/js/main.9e131c71.chunk.js"
+  },
+  {
+    "revision": "72f1e767ee679f868fb9",
+    "url": "/sushi-react/static/js/runtime-main.e1b1b333.js"
+  },
+  {
+    "revision": "201a2d8bdd3fe1b6390aef55096767f2",
+    "url": "/sushi-react/static/media/line.201a2d8b.png"
+  },
+  {
+    "revision": "2435032277d8a3a608864360f17399a9",
+    "url": "/sushi-react/static/media/sushi.24350322.png"
+  }
+]);
