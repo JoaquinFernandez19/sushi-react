b640a468f4ae97c9cb2b47a9db8b17d0511a7e8a
Joaquin Fernandez <copako12@gmail.com>
2020-04-02 19:30:40 -0300

    implementing build

---
+++ build/precache-manifest.3a99fe2974479ccd27cafb6447186198.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3beb6cab4dec57c67e705fb6f111e73",
+    "url": "./index.html"
+  },
+  {
+    "revision": "405749552c1b76fa8ed0",
+    "url": "./static/css/main.33b7dc5f.chunk.css"
+  },
+  {
+    "revision": "09a6c9f827135465de01",
+    "url": "./static/js/2.13d404f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.13d404f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "405749552c1b76fa8ed0",
+    "url": "./static/js/main.9e131c71.chunk.js"
+  },
+  {
+    "revision": "1fd0246e57dc52d30339",
+    "url": "./static/js/runtime-main.678aa708.js"
+  },
+  {
+    "revision": "201a2d8bdd3fe1b6390aef55096767f2",
+    "url": "./static/media/line.201a2d8b.png"
+  },
+  {
+    "revision": "2435032277d8a3a608864360f17399a9",
+    "url": "./static/media/sushi.24350322.png"
+  }
+]);
